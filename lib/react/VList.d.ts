23baf8672ce83abf20c0b5ac7d3dd7ba27cc8b6f
toddtarsi <tarsitodd@gmail.com>
2025-08-28 13:22:55 -0500

    updated lib

--- lib/react/VList.d.ts
+++ lib/react/VList.d.ts
@@ -8,7 +8,7 @@ export interface VListHandle extends VirtualizerHandle {
 /**
  * Props of {@link VList}.
  */
-export interface VListProps extends Pick<VirtualizerProps, "children" | "count" | "overscan" | "itemSize" | "shift" | "horizontal" | "cache" | "ssrCount" | "item" | "onScroll" | "onScrollEnd" | "keepMounted">, ViewportComponentAttributes {
+export interface VListProps extends Pick<VirtualizerProps, "children" | "count" | "overscan" | "itemSize" | "shift" | "horizontal" | "cache" | "ssrCount" | "item" | "onScroll" | "onScrollEnd" | "keepMounted" | "renderPlaceholders">, ViewportComponentAttributes {
     /**
      * If true, items are aligned to the end of the list when total size of items are smaller than viewport size. It's useful for chat like app.
      */
